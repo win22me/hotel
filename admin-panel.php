4a7d47154ea636f0a6d727a21563d9290945b5bd
Ajay <win_2me@act.edu.om>
2025-01-07 13:19:07 +0400

    add customer

--- admin-panel.php
+++ admin-panel.php
@@ -1,11 +1,11 @@
 <?php
 // Database connection
 $conn = new mysqli('localhost', 'root', '', 'booking');
-<h1> List of customer </h1>
+
 // Fetch bookings
 $result = $conn->query("SELECT * FROM bookings ORDER BY created_at DESC");
 
-echo "<h1>Booking List</h1>";
+echo "<h1>Booking Customer List</h1>";
 echo "<table border='1'>";
 echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th><th>Check-In</th><th>Check-Out</th><th>Room Type</th><th>Guests</th><th>Special Requests</th></tr>";
 
